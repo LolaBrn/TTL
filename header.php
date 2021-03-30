2ec7f61429e88ba4b2dccfd343c8995c262f31be
Lola <lolaaa.brn@gmail.com>
2021-03-30 18:55:09 +0200

    premier ajouts header

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <meta name="description" content="Site de jeu The Toria's League. Jeu de cartes en ligne dans un univers médiéval et d'aventures">
+    <link rel="icon" type="image/x-icon" href="img/logo_jeu.png">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<header>
+
+</header>
+</html>
