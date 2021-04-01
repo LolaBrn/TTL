71f25b8e62761a99a3ae473c07091960e643b5a5
Lola <lolaaa.brn@gmail.com>
2021-04-01 14:36:48 +0200

    ajout footer

---
+++ accueil.php
@@ -0,0 +1,12 @@
+<?php require 'header.php'; ?>
+
+<section id="home">
+    <div id="home_connexion">
+        <button id="inscription">INSCRIPTION</button>
+        <button id="connexion">CONNEXION</button>
+    </div>
+</section>
+
+<?php require 'footer.php'?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -5,21 +5,23 @@
     <meta name="description" content="Site de jeu The Toria's League. Jeu de cartes en ligne dans un univers médiéval et d'aventures">
     <link rel="icon" type="image/x-icon" href="img/logo_jeu.png">
     <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/accueil.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
 </head>
-<header>
-    <div id="nav">
-        <nav role="navigation">
-            <div id="nav_left">
-                <img src="img/logo_jeu.png">
-                <a href="#">ACCUEIL</a>
-                <a>|</a>
-                <a href="#">INSCRIPTION</a>
-            </div>
-            <div id="nav_right">
-                <!--<a href="#">Pseudo</a>-->
-                <div id="avatar"></div>
-            </div>
-        </nav>
-    </div>
-</header>
-</html>
+<body>
+    <header>
+        <div id="nav">
+            <nav role="navigation">
+                <div id="nav_left">
+                    <img src="img/logo_jeu.png">
+                    <a href="#">ACCUEIL</a>
+                    <a>|</a>
+                    <a href="#">INSCRIPTION</a>
+                </div>
+                <div id="nav_right">
+                    <!--<a href="#">Pseudo</a>-->
+                    <div id="avatar"></div>
+                </div>
+            </nav>
+        </div>
+    </header>
