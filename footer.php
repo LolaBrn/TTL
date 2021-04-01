9142efccc77bb4cf3369adf1116adf7852285f68
Lola <lolaaa.brn@gmail.com>
2021-04-01 17:40:57 +0200

    footer pas terminé

--- footer.php
+++ footer.php
@@ -1,3 +1,13 @@
 <footer>
-    
+    <div id="footer1">
+        <div id="footer_agence"><img src="img/logo_agence_blanc.png"></div>
+        <div id="footer_jeu"><img src="img/logo_jeu.png"></div>
+        <div id="footer_liens">
+            <a href="politique.html"><p>| Politique de confidentialité</p></a>
+            <a href="condition.html"><p>| CGU - CGV</p></a>
+            <a href="https://mmi19g01.mmi-troyes.fr/Irogames/"><p>| Qui sommes-nous ?</p></a>
+            <a href="#"><p>| Gestion</p></a>
+        </div>
+    </div>
+    <div id="footer2"></div>
 </footer>
