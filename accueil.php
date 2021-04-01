90a3b26af6ea28007c18a3a635a232e647c0a430
Lola <lolaaa.brn@gmail.com>
2021-04-01 20:00:32 +0200

    modif accueil

--- accueil.php
+++ accueil.php
@@ -6,6 +6,24 @@
         <button id="connexion">CONNEXION</button>
     </div>
 </section>
+<section id="home_prez">
+    <div id="prez">
+        <h1>| PRÉSENTATION</h1>
+        <p>Suite à la disparition de son fabuleux trésor, la ville de Toria recherche de l'aide...</p>
+        <p>Aventurier en quête de gloire et richesse, vous décidez d'accepter la quête offerte par la ville et de vous rendre dans le Donjon Noir afin de
+        rapporter le trésor de Toria.</p>
+        <p>Malheureusement, dans votre recherche de mercenaires, un autre aventurier vous mets des bâtons dans les roues en cherchant à recruter les même
+        professionnels que vous</p>
+        <p>Dans un duel au tour par tour, vous allez amasser le plus d'équipement possible afin d'obtenir les faveurs du plus de mercenaire possible.</p>
+    </div>
+    <div id="home_info">
+        <img src="img/people.png">
+        <p>2 joueurs</p>
+        <img src="img/timer.png">
+        <p>20 minutes</p>
+        <p>13 ans et plus</p>
+    </div>
+</section>
 
 <?php require 'footer.php'?>
 </body>
