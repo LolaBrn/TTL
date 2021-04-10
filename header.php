d9698cd0252f8ba2df994b71eeef447d15e6e46e
Lola <lolaaa.brn@gmail.com>
2021-04-10 17:45:25 +0200

    modif header

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <title>Accueil</title>
     <meta name="description" content="Site de jeu The Toria's League. Jeu de cartes en ligne dans un univers médiéval et d'aventures">
-    <link rel="icon" type="image/x-icon" href="img/logo_jeu.png">
+    <link rel="icon" type="image/x-icon" href="img/logo_jeu.ico">
     <link rel="stylesheet" type="text/css" href="css/header.css">
     <link rel="stylesheet" type="text/css" href="css/accueil.css">
     <link rel="stylesheet" type="text/css" href="css/footer.css">
@@ -20,8 +20,8 @@
                     <a href="inscription.php">INSCRIPTION</a>
                 </div>
                 <div id="nav_right">
-                    <!--<a href="#">Pseudo</a>-->
-                    <div id="avatar"></div>
+                    <a href="profil.php">Pseudo</a>
+                    <div id="avatar"><img src="img/avatar3.png"></div>
                 </div>
             </nav>
         </div>
